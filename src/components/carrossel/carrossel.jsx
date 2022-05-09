854cf624464450495eccaf15b90bab05236506c3
Yure Nogueira <nogueirayure1993@gmail.com>
2022-05-09 19:04:36 -0300

    fix: added prop to configure carrossel measures

--- src/components/carrossel/carrossel.jsx
+++ src/components/carrossel/carrossel.jsx
@@ -1,12 +1,12 @@
 import CarrosselFrame from "../carrossel-frame/carrossel-frame";
 import { Title, CarrosselContainer } from "./carrossel.styles";
 
-function Carrossel() {
+function Carrossel({ configs }) {
   return (
     <CarrosselContainer>
       <Title>The best action games on sale!</Title>
 
-      <CarrosselFrame width={"100%"} maxWidth={"800px"} heightRatio={"66%"} />
+      <CarrosselFrame {...configs} />
     </CarrosselContainer>
   );
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,10 +1,16 @@
 import Carrossel from "../../components/carrossel/carrossel";
 import "./homepage.scss";
 
+const CARROSSEL_CONFIGS = {
+  width: "100%",
+  maxWidth: "800px",
+  heightRatio: "66%",
+};
+
 function Homepage() {
   return (
     <div className='homepage-container'>
-      <Carrossel />
+      <Carrossel configs={CARROSSEL_CONFIGS} />
     </div>
   );
 }
