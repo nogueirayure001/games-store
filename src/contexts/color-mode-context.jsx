c4ecd2ec5d536398b1566fccdb59f677150322fe
Yure Nogueira <nogueirayure1993@gmail.com>
2022-05-16 20:30:03 -0300

    feat: color mode now is persistent

--- src/contexts/color-mode-context.jsx
+++ src/contexts/color-mode-context.jsx
@@ -1,16 +1,23 @@
 import { createContext, useState } from "react";
 
+const MODE = "prefferredMode";
+
 export const ColorModeContext = createContext({
   usingDarkMode: true,
   toggleColorMode: () => {},
 });
 
 export function ColorModeContextProvider({ children }) {
-  const [usingDarkMode, setUsingDarkMode] = useState(true);
+  const [usingDarkMode, setUsingDarkMode] = useState(
+    localStorage.getItem(MODE) ? JSON.parse(localStorage.getItem(MODE)) : true
+  );
 
   const value = {
     usingDarkMode: usingDarkMode,
-    toggleColorMode: () => setUsingDarkMode(!usingDarkMode),
+    toggleColorMode: () => {
+      localStorage.setItem(MODE, !usingDarkMode);
+      setUsingDarkMode(!usingDarkMode);
+    },
   };
 
   return (
