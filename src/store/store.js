5f422b303456dd2a2b534071e5b2550450c68ac6
Yure Nogueira <1993nogueirayure@gmail.com>
2022-06-28 03:48:33 -0300

    feat: cart will persist on localstorage through the use of redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,29 @@
 import { compose, applyMiddleware, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import { rootReducer } from "./root.reducer";
 
-const middlewares = [logger];
-const enhancers = compose(applyMiddleware(...middlewares));
+const middlewares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
 
-export const store = createStore(rootReducer, enhancers);
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const enhancers = composeEnhancer(applyMiddleware(...middlewares));
+
+const persistConfigs = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfigs, rootReducer);
+
+export const store = createStore(persistedReducer, enhancers);
+
+export const persistor = persistStore(store);
